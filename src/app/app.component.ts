76790c810b9c0967837891fa7c82bd74c2f0c7a2
TomaszPosluszny <tomkposluszny@gmail.com>
2023-03-27 15:48:56 +0200

    Exercises with style binding and class binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'wsb1';
-  parentMessage: string = "Wiadomość";
+  parentMessage: string = 'Wiadomość';
+  setColor: string = 'blue';
+  setColor2: string = 'red';
+  klasaCss = ['text-color', 'text2-color','text3-color3'] 
+  klasaCss1 = 'text3-color3'
 
-  //TO DODAŁEM:
-  // message:string;
-   
-  // receiveMessage($event: string):void {
-  //   this.message = $event
-  // }
+  ngOnInit(): void {
+    console.log('setColor');
+  }
+  updateColor(): void {
+    this.setColor2 = 'yellow';
+  }
+  updateColor2(): void {
+  	const index = Math.floor(Math.random() * this.klasaCss.length);
+  console.log(this.klasaCss[index])
+  this.klasaCss1 = this.klasaCss[index]
+ 
+    
+  
+  }
 
 
-  ngOnInit(){}
 }
